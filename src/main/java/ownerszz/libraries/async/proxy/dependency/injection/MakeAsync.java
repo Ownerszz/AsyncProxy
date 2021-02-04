6b6568d662f306f5821a54ea6aaf3bec0fedb6b1
Ownerszz <tneownerszz@gmail.com>
2021-02-04 18:44:07 +0100

    Forgot the @Dependency

--- src/main/java/ownerszz/libraries/async/proxy/dependency/injection/MakeAsync.java
+++ src/main/java/ownerszz/libraries/async/proxy/dependency/injection/MakeAsync.java
@@ -1,4 +1,6 @@
 package ownerszz.libraries.async.proxy.dependency.injection;
+import ownerszz.libraries.dependency.injection.core.Dependency;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -6,5 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Dependency
 public @interface MakeAsync {
 }
